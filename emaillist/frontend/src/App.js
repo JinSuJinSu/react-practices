71ea699f65891bba24466967713aa0ad50044824
hjs429 <hjs928@naver.com>
2022-03-24 12:27:06 +0900

    refresh emaillist data

---
+++ emaillist/frontend/src/App.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import './assets/scss/App.scss';
+import RegisterForm from './RegisterForm';
+import SearchBar from './SearchBar';
+import Emaillist from './Emaillist';
+import data from './assets/json/data.json';
+
+const App = () => {
+  const [emails, setEmails] = useState(data);
+
+  useEffect(async () => {
+      const response = await fetch('/api', {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: null
+      });
+
+      console.log(response);
+  }, []);
+
+
+  const notifyKeywordChange = function(keyword) {
+    setEmails(data.filter(e => e.firstName.indexOf(keyword) != -1 || e.lastName.indexOf(keyword) != -1 || e.email.indexOf(keyword) != -1));
+  }
+
+  return (
+    <div className={'App'}>
+      <RegisterForm />
+      <SearchBar callback={notifyKeywordChange}/>
+      <Emaillist emails={emails} />
+    </div>
+  )
+}
+
+export default App
