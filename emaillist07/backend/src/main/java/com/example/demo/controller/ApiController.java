9c4273c9137d7bc310cfe4f8651e3eb70ca59e52
hjs429 <hjs928@naver.com>
2022-04-06 09:54:24 +0900

    pom.xml 수정

--- emaillist07/backend/src/main/java/com/example/demo/controller/ApiController.java
+++ emaillist07/backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -3,9 +3,11 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +15,22 @@ import com.example.demo.dto.JsonResult;
 import com.example.demo.repository.EmaillistRepository;
 import com.example.demo.vo.EmaillistVo;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
+@CrossOrigin(origins={"http://localhost:9090"}, allowedHeaders="*", allowCredentials ="false", 
+methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
+//@CrossOrigin("*")
 public class ApiController {
 	
 	@Autowired
 	private EmaillistRepository emaillistRepository;
 	
+	
 	@GetMapping("/api")
 	public ResponseEntity<JsonResult> read(@RequestParam(value="kw", required=true, defaultValue="") String keyword) {
+//		log.info("Request [GET /api]");
 		return ResponseEntity
 				.status(HttpStatus.OK)
 				.body(JsonResult.success(emaillistRepository.findAll(keyword)));
